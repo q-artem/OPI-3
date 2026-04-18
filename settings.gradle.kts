771eaa887b6ec75171361bfcf82522fd9005085f
RofleX <94610518+RofIeX@users.noreply.github.com>
2026-04-18 19:03:17 +0300

    add ant tempale

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,10 @@
 rootProject.name = "lab3"
+
+tasks.register<Copy>("copyLibs") {
+    // скидываем всё нужное в libs
+    from(configurations.compileClasspath)
+    from(configurations.runtimeClasspath)
+
+    into("$projectDir/lib")
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
